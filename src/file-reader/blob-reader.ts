8dd978495d2c7da604168984d8d17aa6bc531d30
John <moxalibudbud@gmail.com>
2025-04-22 11:01:48 +0400

    add file reader

---
+++ src/file-reader/blob-reader.ts
@@ -0,0 +1,26 @@
+import { ReadStream } from 'fs';
+import { ReadLineBase, ShouldInitiateInterface } from './read-line-base';
+import { createBlobClient } from '@etl/azure-blob';
+
+export class BlobReader extends ReadLineBase implements ShouldInitiateInterface {
+  constructor(url: string) {
+    super(url);
+  }
+
+  async getReadStream(): Promise<ReadStream> {
+    const blobClient = await createBlobClient(this.url);
+    const { readableStreamBody } = await blobClient.download(0);
+    return readableStreamBody as ReadStream;
+  }
+
+  // async readlineInterfacePromise(onLineHandler: any, onCloseHandler: any, onErrorHandler: any): Promise<any> {
+  //   const readStream = await this.getReadStream();
+  //   this.setInterface(readStream);
+  //   return this.readlinePromise(onLineHandler, onCloseHandler, onErrorHandler);
+  // }
+
+  async initiateInterface(): Promise<void> {
+    const readStream = await this.getReadStream();
+    this.setInterface(readStream);
+  }
+}
