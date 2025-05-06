d67151350081998724f68d6414d5f47944af411a
John <moxalibudbud@gmail.com>
2025-05-06 13:11:44 +0400

    Revert "Some comments for line output base"
    This reverts commit e220025604d99c441fd83c9036bd6815c32f18de.

--- src/line-data/line-output.ts
+++ src/line-data/line-output.ts
@@ -1,13 +1,3 @@
-/**
- * TODO list
- *
- * Filename = Instead of passing a function. We can pass an object options and perform the condition based on given options.
- * Examples:
- * If filname.name exist. Means client wants to use fix filename.
- * if filename.template exist. Then client wants a custom filename. If yes, following option must be pass
- * Sample template = `Item Master - {[timestamp]}` = this means [timestamp] is a built in function that generate timestamp
- * Sample template = `Item Master - {[LINE_OBJECT.output.sku]}` = this means LINE_OBJECT.output.sku is a built in function that fetch data from line source class
- */
 type LineOutputBase = {
   filename: string | ((args: any) => string);
   separator: string;
