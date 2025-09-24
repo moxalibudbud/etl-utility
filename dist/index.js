72af5dab5d343011e4460ee5f6eb632c31cd023a
John <moxalibudbud@gmail.com>
2025-09-24 14:20:46 +0400

    0.2.2-beta

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,4 @@ tslib_1.__exportStar(require("./etl"), exports);
 tslib_1.__exportStar(require("./file-generator"), exports);
 tslib_1.__exportStar(require("./line-data"), exports);
 tslib_1.__exportStar(require("./types"), exports);
+tslib_1.__exportStar(require("./utils/cache-index-manager"), exports);
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './etl';
 export * from './file-generator';
 export * from './line-data';
 export * from './types';
+export * from './utils/cache-index-manager';
