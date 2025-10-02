107bbcafed3a858e34f21b1fad7c07c8535625af
John <jalibudbud@altavantconsulting.com>
2025-10-02 20:19:34 +0400

    update cin7-product-sku-octoplus.script.ts

--- src/tests/debug-scripts/etl-utility/configs/cin7-product-sku-octoplus.ts
+++ src/tests/debug-scripts/etl-utility/configs/cin7-product-sku-octoplus.ts
@@ -2,13 +2,17 @@ import { LineOutputOptions, LineSourceBaseOptions } from '../../../../index';
 
 export const line: LineSourceBaseOptions = {
   columns: [],
-  mandatoryFields: ['SKU', 'Barcode', 'Category'],
-  identifierMappings: { barcode: 'Barcode', sku: 'SKU', id: 'ID' },
+  mandatoryFields: ['sku', 'internal_barcode', 'Category'],
+  identifierMappings: {
+    barcode: 'internal_barcode',
+    sku: 'sku',
+    id: 'ID',
+  },
   outputMappings: {
     TYPE_MOUVEMENT: 'M',
-    CODE_ARTICLE: 'SKU',
-    MAIN_EAN: 'Barcode',
-    CODE_PRODUCT: '',
+    CODE_ARTICLE: 'sku',
+    MAIN_EAN: 'internal_barcode',
+    CODE_PRODUCT: 'ID',
     SHORT_LABEL: 'Name',
     LONG_LABEL: '',
     CODE_SEASON: '',
@@ -47,11 +51,7 @@ export const output: LineOutputOptions = {
     template: 'I_SKU_[timestamp].csv',
   },
   header:
-    'TYPE_MOUVEMENT;CODE_ARTICLE;MAIN_EAN;CODE_PRODUCT;SHORT_LABEL;LONG_LABEL;CODE_SEASON;CODE_SUPPLIER;CODE_FAMILY;CODE_NGP;CODE_COLOR;CODE_SIZE;PRIMARY_TYPE_TAG;SECONDARY_TYPE_TAG;SELLING_PRICE;BUYING_PRICE;URL_PICTURE;TRACEABILITY_MODE;SUPPLIER_REF;END_PRODUCT;QUANTITY;UNIT;FIDELITY_PRICE;USERFIELD_1;USERFIELD_2;USERFIELD_3;USERFIELD_4;USERFIELD_5;CODE_BRAND;PRICE_COMPARISON_UNIT;PRICE_COMPARISON_DIVIDER;RANK\n',
-  // footer: 'sample footer',
-  // header: (args: SourceLine) => generatorOptions.columns.join(generatorOptions.separator) + '\n',
-  // footer: 'sample footer',
+    'TYPE_MOUVEMENT;CODE_ARTICLE;MAIN_EAN;CODE_PRODUCT;SHORT_LABEL;LONG_LABEL;CODE_SEASON;CODE_SUPPLIER;CODE_FAMILY;CODE_NGP;CODE_COLOR;CODE_SIZE;PRIMARY_TYPE_TAG;SECONDARY_TYPE_TAG;SELLING_PRICE;BUYING_PRICE;URL_PICTURE;TRACEABILITY_MODE;SUPPLIER_REF;END_PRODUCT;QUANTITY;UNIT;FIDELITY_PRICE;USERFIELD_1;USERFIELD_2;USERFIELD_3;USERFIELD_4;USERFIELD_5;CODE_BRAND;PRICE_COMPARISON_UNIT;PRICE_COMPARISON_DIVIDER;RANK',
   separator: ';',
   uniqueKey: 'SKU',
-  // template: 'false;{BARCODE};{PART_NUMBER};{DESCRIPTION};g',
 };
